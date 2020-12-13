b62e268cbfcd914314c48a91aab5f8c83f55fc15
Xiaosha Xing <xiaosha.xing@logmein.com>
2020-12-13 21:45:00 +0100

    chore: finish IAC-lesson3 networking setup

--- supporting_material/update.sh
+++ supporting_material/update.sh
@@ -1,1 +1,7 @@
-aws cloudformation update-stack --stack-name $1 --template-body file://$2  --parameters file://$3 --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=us-west-2
+#./update.sh my-test-stack myinfra.yml myinfra-params.json
+aws cloudformation update-stack \
+--stack-name $1 \
+--template-body file://$2  \
+--parameters file://$3 \
+--capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" \
+--region=us-west-2
